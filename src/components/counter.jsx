7e120ec1c96fa35fc501e003966d1b6ea83c15c1
paulwoojungkim <wj2kim@gmail.com>
2020-06-05 22:50:51 +0900

    up to dynamic rendering

---
+++ src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  styles = {
+    fontSize: 12,
+    fontWeight: "bold",
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    const x = "Zero";
+    return count === 0 ? x : count;
+  }
+}
+
+export default Counter;
