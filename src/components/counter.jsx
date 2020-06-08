7d81cc2f8640bb8a6a34a371835e8536e9888e9e
paulwoojungkim <wj2kim@gmail.com>
2020-06-08 22:46:25 +0900

    almost finish

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,46 +1,43 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
-
   styles = {
     fontSize: 12,
     fontWeight: "bold",
   };
 
-  handleIncrement = (product) => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
-    console.log(this.props);
-
     return (
       <React.Fragment>
-        <h4>{this.props.id}</h4>
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button
-          onClick={(product) => this.handleIncrement(product)}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
+        <div>
+          <span style={this.styles} className={this.getBadgeClasses()}>
+            {this.formatCount()}
+          </span>
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            Increment
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            Delete
+          </button>
+        </div>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     const x = "Zero";
     return value === 0 ? x : value;
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,22 +2,19 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 3 },
-    ],
-  };
   render() {
+    const { counters, onReset, onDelete, onIncrement } = this.props;
     return (
       <React.Fragment>
-        {this.state.counters.map((counter) => (
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
           ></Counter>
         ))}
       </React.Fragment>
